04e2d273468bde1b69614ca23cb31aef1ba97d66
YACHAE <104hsh76@gmail.com>
2024-04-02 23:37:03 +0900

    4/2 화요일 1~2강까지

---
+++ Lecture_1/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Lecture_1.navigationSplitViewColumnWidth(.navigationSplitViewColumnWidth(/*@START_MENU_TOKEN@*/150/*@END_MENU_TOKEN@*/))
+//
+//  Created by Yachae on 4/2/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var myLabel: UILabel!
+    @IBOutlet weak var Switch: UISwitch!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
